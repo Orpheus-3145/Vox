d094a6a28b3cd3c943e76d2bf524c37774473c87
Soepgroente <117657433+Soepgroente@users.noreply.github.com>
2026-05-10 12:52:30 +0200

    Slightly better setting

--- source/VoxelMapMove.cpp
+++ source/VoxelMapMove.cpp
@@ -99,7 +99,7 @@ vec3	VoxelMap::nearestAirVoxel(const vec3i& origin)
 
 void	VoxelMap::insideVoxels(const vec3& position, std::vector<vec3i>& locations) const noexcept
 {
-	constexpr float playerRadius = 0.48f;
+	constexpr float playerRadius = 0.5f - epsilon();
 	
 	locations.clear();
 	locations.push_back(roundyRound(position));
